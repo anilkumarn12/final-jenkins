3bceba79f97dffb43a6141f0656446354d5f6333
anilkumarn12 <134625092+anilkumarn12@users.noreply.github.com>
2023-07-28 17:58:51 +0530

    Add files via upload

---
+++ Jenkinsfile.tf
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/anilkumarn12/final-jenkins.git']])
+            }
+        }
+
+        stage("terraform init") {
+            steps {
+                sh 'terraform init'
+            }
+        }
+
+        stage("plan") {
+            steps {
+                sh 'terraform plan'
+            }
+        }
+
+        stage("Action") {
+            steps {
+                sh 'terraform apply -auto-approve'
+            }
+        }
+    }
+}
